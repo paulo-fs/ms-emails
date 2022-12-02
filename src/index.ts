482315eb86a1d45070c7676497e70b9b9247d30b
paulo-fs <jr.paulo87@gmail.com>
2022-12-02 15:34:18 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+const route = express.Router()
+
+app.use(express.json())
+app.use(route)
+
+route.get('/', (req, res) => {
+   console.log('teste')
+   res.json({ message: 'hello world!' })
+})
+
+
+app.listen(3000, () => 'server runing on port 3000')
